c64a9c83ca62bf353bc436d0afe440426e4c2223
sofilailatul <lailatulsofi11@gmail.com>
2024-09-20 07:03:01 +0700

    JS 4 - Praktikum 2.2

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -34,19 +34,19 @@ class UserController extends Controller
         //     'password' => Hash::make('12345'),
         // ];
         
-        $user = UserModel::firstWhere('level_id', 1);
-        return view('user', ['data' => $user]);
+        // $user = UserModel::firstWhere('level_id', 1);
+        // return view('user', ['data' => $user]);
 
-        $user = UserModel::findOr(1, ['username', 'nama'], function () {
-            abort(404);
-        });
-        return view('user', ['data' => $user]);
+        // $user = UserModel::findOr(1, ['username', 'nama'], function () {
+        //     abort(404);
+        // });
+        // return view('user', ['data' => $user]);
 
         // $user = UserModel::findOrFail(1);
         // return view('user', ['data' => $user]);
 
-        // $user = UserModel::where('username', 'manager9')->firstOrFail();
-        // return view('user', ['data' => $user]);
+        $user = UserModel::where('username', 'manager9')->firstOrFail();
+        return view('user', ['data' => $user]);
 
         // $user = UserModel::where('level_id', 2)->count();
         // return view('user', ['data' => $user]);
