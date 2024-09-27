3299bdded28ebd2327707ff9a65696e0f32b7533
sofilailatul <lailatulsofi11@gmail.com>
2024-09-27 08:19:00 +0700

    JS 5 - Praktikum 4

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -22,7 +22,8 @@ class UserController extends Controller
         ];
 
         $activeMenu = 'user'; // set menu yang sedang aktif
-        return view('user.index', ['breadcrumb' => $breadcrumb, 'page' => $page, 'activeMenu' => $activeMenu]);
+        $level = LevelModel::all(); //ambil data level untuk filter level
+        return view('user.index', ['breadcrumb' => $breadcrumb, 'page' => $page, 'level' => $level ,'activeMenu' => $activeMenu]);
     }
 
     // Ambil data user dalam bentuk json untuk datables
@@ -30,6 +31,11 @@ class UserController extends Controller
     {
         $users = UserModel::select('user_id', 'username', 'nama', 'level_id')
             ->with('level');
+
+        // Filter data user berdasarkan level_id
+        if ($request->level_id) {
+            $users = $users->where('level_id', $request->level_id);
+        } 
         return DataTables::of($users)
             // menambahkan kolom index / no urut (default nama kolom: DT_RowIndex)
             ->addIndexColumn()
--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -1,4 +1,5 @@
 @extends('layouts.template')
+
 @section('content')
 <div class="card card-outline card-primary">
     <div class="card-header">
@@ -18,6 +19,22 @@
                 {{ session('error') }}
             </div>
         @endif
+        <div class="row">
+            <div class="col-md-12">
+                <div class="form-group row">
+                    <label class="col-1 control-label col-form-label">Filter:</label>
+                    <div class="col-3">
+                        <select class="form-control" id="level_id" name="level_id" required>
+                            <option value="">- Semua -</option>
+                            @foreach ($level as $item)
+                                <option value="{{ $item->level_id }}">{{ $item->level_nama }}</option>
+                            @endforeach
+                        </select>
+                    </div>
+                    <small class="form-text text-muted">Level Pengguna</small>
+                </div>
+            </div>
+        </div>
         <table class="table table-bordered table-striped table-hover table-sm" id="table_user">
             <thead>
                 <tr>
@@ -32,8 +49,10 @@
     </div>
 </div>
 @endsection
+
 @push('css')
 @endpush
+
 @push('js')
     <script>
         $(document).ready(function() {
@@ -43,7 +62,10 @@
                 ajax: {
                     "url": "{{ url('user/list') }}",
                     "dataType": "json",
-                    "type": "POST"
+                    "type": "POST",
+                    "data": function (d) {
+                        d.level_id = $('#level_id').val();
+                    }
                 },
                 columns: [{
                     // nomor urut dari laravel datatable addIndexColumn()
@@ -75,6 +97,10 @@
                     orderable: false,
                     searchable: false
                 }]
+            });
+
+            $('#level_id').on('change', function() {
+                dataUser.ajax.reload();
             });
         });
     </script>
