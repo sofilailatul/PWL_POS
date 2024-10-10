9996a8de913090a39f3fb4038c41d0d4f9f1720b
sofilailatul <lailatulsofi11@gmail.com>
2024-10-11 06:57:23 +0700

    JS 7 - Praktikum 3

---
+++ app/Http/Middleware/AuthorizeUser.php
@@ -0,0 +1,22 @@
+<?php
+namespace App\Http\Middleware;
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+class AuthorizeUser
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next, $role = ''): Response
+    {
+        $user = $request->user(); //mengambil data user yang login
+        if($user->hasRole($role)) {
+            return $next($request);
+        }
+        //jika tidak punya role, maka tampilkan error 403
+        abort(403, 'Forbidden. Kamu tidak punya akses ke halaman ini');
+    }
+}
--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -2,25 +2,30 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Foundation\Auth\User as Authenticatable; // implementas class Authenticatable
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class UserModel extends Authenticatable
 {
     use HasFactory;
-    protected $table = 'm_user'; // Mendefinisikan nama tabel
-    protected $primaryKey = 'user_id'; // Mendefinisikan primary key
+
+    protected $table = 'm_user';        // Mendefinisikan nama tabel yang digunakan oleh model ini
+    protected $primaryKey = 'user_id';  //Mendefinisikan primary key dari tabel yang digunakan
+
     protected $fillable = ['level_id', 'username', 'nama', 'password'];
 
-    protected $hidden = ['password'];
-
+    protected $hidden = ['password']; 
     protected $casts = ['password' => 'hashed'];
 
-    // Relasi ke LevelModel (belongsTo)
-    public function level(): BelongsTo
-    {
+    public function level():BelongsTo {
         return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
+    }
+
+    public function geRoleName(): string {
+        return $this->level->level_nama;
+    }
+    public function hasRole($role): bool {
+        return $this->level->level_kode == $role;
     }
 }
