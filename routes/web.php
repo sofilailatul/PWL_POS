27b5c0999d1d3990214d376ca7fa5d3c9b44c238
sofilailatul <lailatulsofi11@gmail.com>
2024-10-18 07:43:25 +0700

    JS 8 - Praktikum 2

--- routes/web.php
+++ routes/web.php
@@ -96,6 +96,7 @@ Route::middleware(['auth'])->group(function () {
         Route::delete('/{id}', [BarangController::class, 'destroy']); // menghapus data barang
         Route::get('/import', [BarangController::class, 'import']); // ajax form upload excel
         Route::post('/import_ajax', [BarangController::class, 'import_ajax']); // ajax import excel
+        Route::get('/export_excel', [BarangController::class, 'export_excel']); // export excel
     });
 
     //route supplier
