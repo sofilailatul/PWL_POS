6d436f423ed0d7f7010b44b887c1a7c182d1286b
sofilailatul <lailatulsofi11@gmail.com>
2024-11-06 14:39:41 +0700

    JS 10 - Praktikum 1

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -4,11 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class UserModel extends Authenticatable
+class UserModel extends Authenticatable implements JWTSubject
 {
-    use HasFactory;
+    public function getJWTIdentifier() {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims() {
+        return [];
+    }
 
     protected $table = 'm_user';        // Mendefinisikan nama tabel yang digunakan oleh model ini
     protected $primaryKey = 'user_id';  //Mendefinisikan primary key dari tabel yang digunakan
@@ -30,5 +38,12 @@ class UserModel extends Authenticatable
     }
     public function getRole() {
         return $this->level->level_kode;
+    }
+
+    public function stok(): HasMany {
+        return $this->hasMany(StokModel::class, 'user_id', 'user_id');
+    }
+    public function penjualan(): HasMany {
+        return $this->hasMany(PenjualanModel::class, 'user_id', 'user_id');
     }
 }
